cc619e80db8fb7bb22d260c2dc5139261de0d27f
vipulgoyalengr <52689398+vipulgoyalengr@users.noreply.github.com>
2022-09-14 11:07:46 +0530

    Update passport-setup.js

--- backend/passport-setup.js
+++ backend/passport-setup.js
@@ -14,8 +14,8 @@ const SpotifyStrategy = require('passport-spotify').Strategy;
 // });
 
 passport.use(new GoogleStrategy({
-    clientID: "976963799012-udv4kadvcn82s5gbubslsqftlafs9m6j.apps.googleusercontent.com",
-    clientSecret: "pU5mspEg_jqCTH5wA2dMINH4",
+    clientID: "OAuth_clientID",
+    clientSecret: "OAuth_Secret",
     callbackURL: "http://localhost:2001/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -26,18 +26,18 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: '6558356df2b243cc92b959b6a0ed57c2',
-      clientSecret: 'aed4437c1f9a4096997529059c9aaa72',
-      callbackURL: 'http://localhost:2001/spotify/callback'
-    },
-    function(accessToken, refreshToken, expires_in, profile, done) {
-      console.log(profile)
-      // User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
-      //   return done(err, user);
-      // });
+// passport.use(
+//   new SpotifyStrategy(
+//     {
+//       clientID: '6558356df2b243cc92b959b6a0ed57c2',
+//       clientSecret: 'aed4437c1f9a4096997529059c9aaa72',
+//       callbackURL: 'http://localhost:2001/spotify/callback'
+//     },
+//     function(accessToken, refreshToken, expires_in, profile, done) {
+//       console.log(profile)
+//       // User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
+//       //   return done(err, user);
+//       // });
     }
   )
 );
